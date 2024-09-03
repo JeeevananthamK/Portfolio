dce33dd808ed55cdb5ea1ae17bf358e474f38c15
JeeevananthamK <jeevakumar712@outlook.com>
2024-09-03 23:01:51 +0400

    Add files via upload

---
+++ src/src/components/About/Toolstack.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiSlack,
+  SiVercel,
+  SiWindows,
+  SiKalilinux,
+  SiLinux,
+  SiUbuntu,
+  SiAmazonaws,
+  SiGooglecloud,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiWindows />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiKalilinux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAmazonaws />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiUbuntu />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
---
+++ src/src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Security Analyst",
+          "CTF Player",
+          "Blue Teamer",
+          "DFIR Investigator"
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
